ce025841dd9cb82b37db04142b93c4fadcd790c3
mArcio-Lmano <marcio-mano2322@gmail.com>
2022-06-21 19:06:11 +0100

    Add New VQC struct and Noise Simulation

--- amplitude_encode.py
+++ amplitude_encode.py
@@ -1,8 +1,10 @@
 import sys
 import numpy as np
 import pandas as pd
+from sklearn.decomposition import PCA
 
-identifier = sys.argv[1]
+identifier = sys.argv[2]
+PCA_num_feature = int(sys.argv[1])
 data_set = pd.read_csv(f"Usable_data_sets/{identifier}_data_set.csv")
 data_set.rename(columns={list(data_set)[0]:"OldIndex"}, inplace=True)
 FEATURES_NAMES = list(data_set)[2:]
@@ -24,41 +26,49 @@ def get_values(index):
 		
 	return values, classe
 
-def normalize(lst_values):
+def normalize(array):
+    r = array.copy()
+    for i, datapoint in enumerate(array):
+        soma_sq = sum([x for x in datapoint])
+        r[i] /= soma_sq
+    return r
 
-	soma_sq = sum([x**2 for x in lst_values])
-	normalize_values = np.array(lst_values)
-	normalize_values /= np.sqrt(soma_sq)
-
-	return normalize_values
+def normalize_amp(array):
+    r = array.copy()
+    for i, datapoint in enumerate(array):
+        soma_sq = sum([x**2 for x in datapoint])
+        r[i] /= np.sqrt(soma_sq)
+    return r
 
 def padding(values, num_qubits):
-	if len(values) == num_qubits**2:
-		print("PAD não necessário")
-	else:
-		values_pad = np.zeros(num_qubits**2)
-		for i in range(NUM_FEATURES):
-			values_pad[i] = values[i]
-
-	return values_pad
+    if len(values[0]) == num_qubits**2:
+        print("PAD não necessário")
+    else:
+        for datapoint in values:
+            datapoint_pad = np.zeros(num_qubits**2)
+            for i in range(NUM_FEATURES):
+                datapoint_pad[i] = datapoint[i]
+            datapoint = datapoint_pad
+    return values
 
 def main():
+    data_set_features = data_set.loc[:, FEATURES_NAMES]
+    classes = data_set.loc[:, ["CLASSE"]].values
+    np_data_set_features = data_set_features.to_numpy()
 
-	global COUNTER
-	norm_data_set = np.empty([NUM_SAMPLES, 4**2])
-	classes = np.empty([1,NUM_SAMPLES])
-	while COUNTER < NUM_SAMPLES:
-		values, classe = get_values(COUNTER)
-		values = normalize(values)
-		values = padding(values, 4)
-		norm_data_set[COUNTER] = values
-		classes[0][COUNTER] = classe  
-		COUNTER += 1
+    if PCA_num_feature**2 < 13: 
+        norm_data_set_features = normalize(np_data_set_features)
+        pca = PCA(n_components=PCA_num_feature**2)
+        np_data_set_features = pca.fit_transform(norm_data_set_features)
+        
+    classes_encode = [-1 if classe == "W" else 1 for classe in classes]
+    np_data_set_features_std = normalize_amp(np_data_set_features)
+    np_data_set_features_std = padding(np_data_set_features_std, PCA_num_feature)
 
-	np.savetxt(f"Encode_data/amp_enc_data_set_{identifier}_values.csv", norm_data_set, delimiter=";")
-	np.savetxt(f"Encode_data/amp_enc_data_set_{identifier}_classes.csv", classes, delimiter=";")
-	print(f"Amplitude Enconding guardado em \"Encode_data/amp_enc_data_set_{identifier}_values.csv\"")
-	print(f"Amplitude Enconding guardado em \"Encode_data/amp_enc_data_set_{identifier}_classes.csv\"")
+    np.savetxt(f"Encode_data/amp_enc_data_set_{identifier}_values.csv", np_data_set_features_std, delimiter=";")
+    np.savetxt(f"Encode_data/amp_enc_data_set_{identifier}_classes.csv", classes_encode, delimiter=";")
+    print(f"Amplitude Enconding guardado em \"Encode_data/amp_enc_data_set_{identifier}_values.csv\"")
+    print(f"Amplitude Enconding guardado em \"Encode_data/amp_enc_data_set_{identifier}_classes.csv\"")
 
 if __name__ == '__main__':
 	main()
--- angle_encoding.py
+++ angle_encoding.py
@@ -5,8 +5,8 @@ from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 
 
-identifier = sys.argv[1]
-PCA_num_features = int(sys.argv[2])
+identifier = sys.argv[2]
+PCA_num_features = int(sys.argv[1])
 data_set = pd.read_csv(f"Usable_data_sets/{identifier}_data_set.csv")
 data_set.rename(columns={list(data_set)[0]:"OldIndex"}, inplace=True)
 FEATURES_NAMES = list(data_set)[2:]
@@ -14,32 +14,38 @@ COUNTER = 0
 NUM_FEATURES = 12
 NUM_SAMPLES = len(data_set)
 
-def normalize(array):
+def normalize_ang(array):
     r = array.copy()
     for i, elem in enumerate(array):
-        maxi = max(elem)
+        maxi = max([abs(x) for x in elem])
         for j, num in enumerate(elem):
             r[i][j] = num * np.pi / maxi
-
     return r            
+
+def normalize(array):
+    r = array.copy()
+    for i, datapoint in enumerate(array):
+        soma_sq = sum([x for x in datapoint])
+        r[i] /= soma_sq
+    return r
+
 def main():
 
     data_set_features = data_set.loc[:, FEATURES_NAMES]
     classes = data_set.loc[:,["CLASSE"]].values
 
-    #data_set_features_std = StandardScaler().fit_transform(data_set_features)
-    rdc_data_set_features = data_set_features.to_numpy() 
-    #if PCA_num_features != 12:
+    np_data_set_features = data_set_features.to_numpy() 
 
-    print(PCA_num_features)
     if PCA_num_features != 12: 
+        np_data_set_features = normalize(np_data_set_features)
+        #print(np_data_set_features)
         pca = PCA(n_components=PCA_num_features)
-        rdc_data_set_features = pca.fit_transform(data_set_features)
-    print(rdc_data_set_features)
-    rdc_data_set_features_std = normalize(rdc_data_set_features)
-    classes_encode = [-1 if classe == "W" else 1 for classe in classes]
+        np_data_set_features = pca.fit_transform(np_data_set_features)
 
-    np.savetxt(f"Encode_data/ang_enc_data_set_{identifier}_values.csv", rdc_data_set_features_std, delimiter=";")
+    classes_encode = [-1 if classe == "W" else 1 for classe in classes]
+    np_data_set_features_std = normalize_ang(np_data_set_features)
+    
+    np.savetxt(f"Encode_data/ang_enc_data_set_{identifier}_values.csv", np_data_set_features_std, delimiter=";")
     np.savetxt(f"Encode_data/ang_enc_data_set_{identifier}_classes.csv", classes_encode, delimiter=";")
     print(f"Amplitude Enconding guardado em \"Encode_data/ang_enc_data_set_{identifier}_values.csv\"")
     print(f"Amplitude Enconding guardado em \"Encode_data/ang_enc_data_set_{identifier}_classes.csv\"")        
